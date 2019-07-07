622f279aca905990825215094683fc70dc285d19
Koray Akan <korayakan@zoho.com>
2019-07-07 15:17:48 +0200

    code cleanup

--- module/predict.py
+++ module/predict.py
@@ -1,23 +1,6 @@
 from . import data, net
 
 
-def old_predict(filename, n_predictions=3):
-    #output = evaluate(Variable(lineToTensor(line)))
-
-    # Get top N categories
-    #topv, topi = output.data.topk(n_predictions, 1, True)
-    #predictions = []
-
-    #for i in range(n_predictions):
-    #    value = topv[0][i]
-    #    category_index = topi[0][i]
-    #    print('(%.2f) %s' % (value, all_categories[category_index]))
-    #    predictions.append([value, all_categories[category_index]])
-
-    #return predictions
-    print('')
-
-
 def predict(filename):
     print('File:')
     print(filename, '\n')
